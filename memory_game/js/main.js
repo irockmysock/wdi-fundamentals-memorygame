4303e3e6cd07f91be326f56173505b2ef2692653
Benny Chin <bennychin@bennys-air.lan>
2019-06-12 05:22:00 +0800

    added objects in array

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,40 @@
 console.log("Up and running!")
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length < 2) {
 		return;
 	}
@@ -21,7 +42,10 @@ var flipCard = function(cardId) {
 }
 
 flipCard(0);
+console.log(cards[0].cardImage);
+console.log(cards[0].suit);
+
 flipCard(2);
-
-
+console.log(cards[2].cardImage);
+console.log(cards[2].suit);
 
